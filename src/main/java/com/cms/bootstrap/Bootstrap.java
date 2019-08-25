05a9b3653ecfcd68b2cba86b3f2c6bcdea6e8286
andy <tianhao119163@163.com>
2019-08-25 13:53:42 +0800

    add postgres

--- src/main/java/com/cms/bootstrap/Bootstrap.java
+++ src/main/java/com/cms/bootstrap/Bootstrap.java
@@ -7,11 +7,6 @@ import com.blade.mvc.Const;
 import com.zaxxer.hikari.HikariDataSource;
 import io.github.biezhi.anima.Anima;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.taskdefs.SQLExec;
-import org.apache.tools.ant.types.EnumeratedAttribute;
-
-import java.io.File;
 
 /**
  * @author andy
@@ -20,48 +15,23 @@ import java.io.File;
 @Bean
 public class Bootstrap implements BladeLoader {
 
-    private static String DBTYPE = "H2";
-    private enum Driver{
-        /**
-         * H2
-         */
-        H2DRIVER("org.h2.Driver");
-
-        private String driverClass;
-        Driver(String driverClass) {
-            this.driverClass = driverClass;
-        }
-    }
-
     @Override
     public void preLoad(Blade blade) {
         HikariDataSource ds = new HikariDataSource();
-        ds.setDriverClassName(Driver.H2DRIVER.driverClass);
-        ds.setJdbcUrl(blade.environment().getOrNull("h2.mem"));
-        ds.setUsername("sa");
-        ds.setPassword("");
+        ds.setDriverClassName(blade.environment().getOrNull("jdbc.driver"));
+        ds.setJdbcUrl(blade.environment().getOrNull("jdbc.url"));
+        ds.setUsername(blade.environment().getOrNull("jdbc.user"));
+        ds.setPassword(blade.environment().getOrNull("jdbc.pass"));
         ds.setConnectionTestQuery("select 1 from dual");
-//        Anima.open(ds);
+        //Anima.open(ds);
 
         String DB_PATH = Const.CLASSPATH  + "vcms.db";
         Anima.open("jdbc:sqlite:" + DB_PATH);
-
 
     }
 
     @Override
     public void load(Blade blade) {
         log.info("{},start server.....{}", "2018","");
-        SQLExec sqlExec = new SQLExec();
-        //设置数据库参数
-        sqlExec.setDriver(Driver.H2DRIVER.driverClass);
-        sqlExec.setUrl(blade.environment().getOrNull("h2.mem"));
-        sqlExec.setUserid("sa");
-        sqlExec.setPassword("");
-        sqlExec.setSrc(new File(Const.CLASSPATH+"schema.sql"));
-        sqlExec.setOnerror((SQLExec.OnError)(EnumeratedAttribute.getInstance(SQLExec.OnError.class, "abort")));
-        sqlExec.setPrint(false);
-        sqlExec.setProject(new Project());
-        sqlExec.execute();
     }
 }
